51d353481b05ed4074d3c012db958fd441de38f0
azhardevops50 <azhardevops50@gmail.com>
2025-02-11 16:59:29 +0400

    Add files via upload

---
+++ final-code/aws-eks-demo-cluster/iam.tf
@@ -0,0 +1,74 @@
+resource "aws_iam_role" "ng-role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.ng-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.ng-role.name
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.ng-role.name
+}
+
+resource "aws_iam_role" "my_app_role" {
+  name = "my-app-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "eks.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "my_app_policy" {
+  name        = "my-app-policy"
+  description = "Policy for accessing SSM parameters"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ssm:GetParametersByPath",
+          "ssm:GetParameter",
+          "ssm:Decrypt"
+        ]
+        Resource = "arn:aws:ssm:us-east-1:123456789012:parameter/my-app/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "my_app_policy_attachment" {
+  policy_arn = aws_iam_policy.my_app_policy.arn
+  role       = aws_iam_role.my_app_role.name
+}
+
+
